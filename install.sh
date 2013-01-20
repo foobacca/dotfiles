bbae8c6a41b2b13b8cc955a45d9b7bda1594bb52
Hamish Downer <dmishd@gmail.com>
2013-01-20 22:44:52 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+if [ -d ~/.dotfiles ]; then
+  if type -t git-up > /dev/null; then
+    (cd ~/.dotfiles && git-up)
+  else
+    (cd ~/.dotfiles && git pull --rebase)
+  fi
+else
+  git clone git://github.com/twe4ked/dotfiles.git ~/.dotfiles
+fi
+
+[ -e ~/.freshrc ] || ln -sv ~/.dotfiles/freshrc ~/.freshrc
+
+if [ -e ~/.fresh/build/shell.sh ]; then
+  source ~/.fresh/build/shell.sh
+  fresh
+else
+  bash -c "`curl -sL get.freshshell.com`"
+fi
+
+if ! [ -d ~/.vim/bundle/vundle ]; then
+  git clone git://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
+# vim -N -u ~/.vimrc -s <(cat <<-EOF
+# :set buftype=nofile
+# iRun \`:qa\` to finish install when bundle completes.
+# :BundleClean!
+# :BundleInstall!
+# EOF
+# )
