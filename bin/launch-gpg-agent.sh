98406b3e413d043f726d5e8eb1f99ddc39b7d4b6
Hamish Downer <dmishd@gmail.com>
2013-06-08 10:36:32 +0100

    try again to get gpg-agent script working

--- bin/launch-gpg-agent.sh
+++ bin/launch-gpg-agent.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 if test -f $HOME/.gpg-agent-info &&    kill -0 ‘cut -d: -f 2 $HOME/.gpg-agent-info‘ 2>/dev/null; then
     echo "gpg-agent already running"
+    export $(cat .gpg-agent-info)
      #source "$HOME/.gpg-agent-info"
      #export GPG_AGENT_INFO
      #export SSH_AUTH_SOCK
      #export SSH_AGENT_PID
 else
      #eval 'gpg-agent --daemon --enable-ssh-support --write-env-file'
-     eval 'gpg-agent --daemon --write-env-file'
+     eval $(gpg-agent --daemon --write-env-file)
 fi
 
