326c3cf0caaa4cac3ae428b84f38a6c614917817
Hamish Downer <dmishd@gmail.com>
2013-08-24 10:58:08 +0000

    alot: add hook to check for attachments

---
+++ alot/hooks.py
@@ -0,0 +1,10 @@
+from twisted.internet.defer import inlineCallbacks
+import re
+
+@inlineCallbacks
+def pre_envelope_send(ui, dbm):
+    e = ui.current_buffer.envelope
+    if re.match('.*[Aa]ttach', e.body, re.DOTALL) and not e.attachments:
+        msg = 'no attachments. send anyway?'
+        if not (yield ui.choice(msg, select='yes')) == 'yes':
+            raise Exception()
