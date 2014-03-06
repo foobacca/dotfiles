c53a3c1dd9151248600fcdfaa38e90a65e9618fc
Hamish Downer <dmishd@gmail.com>
2014-03-06 23:11:45 +0000

    alot: Add extra arg to hook

--- alot/hooks.py
+++ alot/hooks.py
@@ -1,8 +1,9 @@
 from twisted.internet.defer import inlineCallbacks
 import re
 
+
 @inlineCallbacks
-def pre_envelope_send(ui, dbm):
+def pre_envelope_send(ui, dbm, cmd):
     e = ui.current_buffer.envelope
     if re.match('.*[Aa]ttach', e.body, re.DOTALL) and not e.attachments:
         msg = 'no attachments. send anyway?'
