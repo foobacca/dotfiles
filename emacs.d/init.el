48a15f279f417dcccd66ebd0425aced14acee88f
Hamish Downer <dmishd@gmail.com>
2014-08-29 19:13:06 +0100

    add initial emacs config

---
+++ emacs.d/init.el
@@ -0,0 +1,17 @@
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+    ("marmalade" . "http://marmalade-repo.org/packages/")
+    ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+; Use this only when installing wiithout ELPA/el-get
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
+; notmuch related stuff
+(autoload 'notmuch' "notmuch" "notmuch mail" t)
+; notmuch runs on a server, we use this script
+(setq notmuch-command "~/bin/remote-notmuch.sh")
+; close the email compose buffer when we've sent it
+(setq message-kill-buffer-on-exit t)
+
+(server-start)
