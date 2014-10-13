e19229ee15bfb9b8aaba69cc5fc8e0cd563f1830
Hamish Downer <dmishd@gmail.com>
2014-10-13 23:13:52 +0100

    emacs notmuch: fcc/bcc and autocomplete

--- emacs.d/notmuch-config.el
+++ emacs.d/notmuch-config.el
@@ -1,1 +1,13 @@
 (setq notmuch-command "/home/hamish/bin/notmuch")
+
+; bcc emails to self, as the emails live on a different machine
+(setq notmuch-fcc-dirs nil)
+(add-hook 'message-header-setup-nook
+    (lambda () (insert (format "Bcc: %s <%s>\n"
+                (notmuch-user-name)
+                (notmuch-user-primary-email)))))
+
+; address autocomplete
+(require 'notmuch-address)
+(setq notmuch-address-command "/home/hamish/bin/notmuch-abook-lookup")
+(notmuch-address-message-insinuate)
