24862caa6aa40bef13538f475c630f608291fa3a
Hamish Downer <hamish@foobacca.co.uk>
2024-11-26 13:43:12 +0000

    lazyvim: disable auto copy to system clipboard

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,4 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- normal line numbering!
 vim.opt.relativenumber = false
+
+-- don't auto copy to the system clipboard
+vim.opt.clipboard = ""
