ab923a7c54abe994433cbd46fac7513147487b00
Hamish Downer <hamish@sortitionfoundation.org>
2025-06-26 10:28:13 +0100

    change lazyvim to use ruff formatter instead of black

--- lazyvim/lua/plugins/example.lua
+++ lazyvim/lua/plugins/example.lua
@@ -102,5 +102,15 @@ if true then
 				})
 			end,
 		},
+
+		{
+			"stevearc/conform.nvim",
+			optional = true,
+			opts = {
+				formatters_by_ft = {
+					["python"] = { "ruff_format" },
+				},
+			},
+		},
 	}
 end
