af414ac4f8616f4376063368b94107fba3b0f638
Hamish Downer <hamish@sortitionfoundation.org>
2026-02-09 13:39:15 +0000

    try basedpyright as nvim python LSP

--- lazyvim/lua/plugins/example.lua
+++ lazyvim/lua/plugins/example.lua
@@ -23,8 +23,10 @@ if true then
 				---@type lspconfig.options
 				servers = {
 					-- pyright will be automatically installed with mason and loaded with lspconfig
-					pyright = {},
+					-- pyright = {},
+					basedpyright = {},
 					ruff_lsp = {},
+					-- ty = {},
 				},
 			},
 		},
